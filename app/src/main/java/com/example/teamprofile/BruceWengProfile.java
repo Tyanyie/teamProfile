830901c1588bc203712516eb4aee40d16a2f87ec
hommula <bruce2588@gmail.com>
2021-02-06 12:31:16 -0800

    adding Intent

---
+++ app/src/main/java/com/example/teamprofile/BruceWengProfile.java
@@ -0,0 +1,14 @@
+package com.example.teamprofile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BruceWengProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bruce_weng_profile);
+    }
+}
